fe2abdbc554f85a699a26dc5cc21b589bdee721b
Abdallh Oudaa <bnaoda18@gmail.com>
2025-03-05 11:33:10 +0200

    DI

--- lib/persentation-layer/screens/categroy-detailes.dart
+++ lib/persentation-layer/screens/categroy-detailes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
+
 import 'package:news_app/Di/di.dart';
 import 'package:news_app/bussinese-logic-layer/sources-view-model.dart';
 
